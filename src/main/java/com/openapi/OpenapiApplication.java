588ee45fdda049f785aee025057cc73297491a63
bvidaltorres <bastianvidal1998@gmail.com>
2022-11-18 13:26:33 -0300

    Initial commit

---
+++ src/main/java/com/openapi/OpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@EnableAuthorizationServer
+@SpringBootApplication
+public class OpenapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiApplication.class, args);
+	}
+}
